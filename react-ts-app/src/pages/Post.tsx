01743273dcd8bfff946df5a5df0af9bf95e5c0e0
akmasui <fmosquitt@gmail.com>
2020-09-07 01:28:44 +0000

    test connection from backend

--- react-ts-app/src/pages/Post.tsx
+++ react-ts-app/src/pages/Post.tsx
@@ -1,31 +1,52 @@
 import React, { useState } from "react";
+import { createTest, createPost } from "./../api"
+
 
 const Home: React.FC = () => {
+  const [mealId, setMealID] = useState(0);
+  const [message, setMessage] = useState("")
+  const [res, setRes] = useState<any>();
 
-  const [values, setValues] = useState({
-    meal_name1: "",
-    meal_name2: "",
-    meal_name3: "",
-    meal_name4: "",
-    meal_name5: "",
-    post_comment: "",
-  });
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
+  // return (
+  //   <div>
+  //     <h1>Post Page</h1>
+  //     <form onSubmit={onSubmit} />
+  // return (
+  //     <div  >
+  //       <h1>Post Page</h1>
+  //       <form onSubmit={onSubmit}>
+  //         <div>
+  //           <select >
+  //             <option onSelect={() => setMealID(1)}>餃子</option>
+  //             <option onSelect={() => setMealID(2)}>カレー</option>
+  //           </select>
+  //         </div>
+  //         <textarea onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value)} />
+  //         <div>
+  //           <button >投稿</button>
+  //         </div>
+  //       </form>
+  //     </div>
+  // )
+
+  // 
+  // {"cuisine": "cuisine", "recipe": "recipe"}
+
+  const getMeal = () => {
+    let temp_res = createTest();
+    temp_res.then(item => setRes(item))
+    console.log(temp_res)
+
+  }
 
   return (
-    <div  >
-      <h1>Post Page</h1>
-      <form>
-        <div>
-          <select >
-            <option>餃子</option>
-            <option>カレー</option>
-          </select>
-        </div>
-        <textarea />
-        <div>
-          <button>投稿</button>
-        </div>
-      </form>
+    <div>
+      <button onClick={getMeal}>Test</button>
+      {res && res.cuisine}
     </div>
   )
 }
