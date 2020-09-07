cd7a9aef3cf7af7827a056ca5b49c4b6d17cee4f
akmasui <fmosquitt@gmail.com>
2020-09-07 12:57:12 +0000

    create useful components

--- react-ts-app/src/components/Header.tsx
+++ react-ts-app/src/components/Header.tsx
@@ -1,17 +1,28 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import PersonIcon from '@material-ui/icons/Person';
+import HomeIcon from '@material-ui/icons/Home';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
+import LensIcon from '@material-ui/icons/Lens';
+
 
 const Header: React.FC = () => {
   return (
-    <nav>
-      <ul>
-        <li>検索</li>
-        <li>王冠</li>
-        <li>カメラ</li>
-        <li>メダル</li>
-        <li>ユーザー（マイページ）</li>
-        <li>ホーム（タイムライン）</li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li>検索</li>
+          <li><Link to="/ranking"><AssessmentIcon />ランキング</Link></li>
+          <li><Link to="/post"><CameraAltIcon />カメラ（投稿）</Link></li>
+          <li><Link to="/mystatus"><LensIcon />メダル</Link></li>
+          <li><Link to="/mypage"><PersonIcon />ユーザー（マイページ）</Link></li>
+          <li><Link to="timeline"><HomeIcon />ホーム（タイムライン）</Link></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
